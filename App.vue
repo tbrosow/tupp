d16f28cb1e11d9eadb244ead3f73cb3d00fd0677
Torsten Brosow <torsten.brosow@gmail.com>
2018-11-26 11:48:25 +1100

    no message

---
+++ App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="full-width center-content">
+        <div>Hello World! {{ name }} WHAT</div>
+        <div>Hello World! {{ test }} WHAT</div>
+        <br/>
+        <button v-on:click="say('')">Say what</button>
+        <button v-on:click="remove('')">Remove</button>
+        <a href="#" v-on:click="say()">test</a>
+
+    </div>
+ </template>
+
+<script>
+    export default {
+        //console.log("DDD");
+        // empty
+        name: 'app',
+        data: function() {
+            return {
+                name:"init data",
+                test: {test: 123}
+
+            }
+        },
+
+        computed: {
+
+        },
+        methods: {
+            say: function (message) {
+                this.message = 'New Date: '+ new Date()
+                alert(this.message)
+                this.name = this.message;
+            },
+            remove: function() {
+                this.name = "";
+            }
+        }
+    }
+
+</script>
---
+++ src/app.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from '../App.vue'
+
+import '../css/test.styl'
+
+import TB from '../src/tb.vue'
+
+new Vue({
+     render: h => h(App)
+}).$mount('#app')
